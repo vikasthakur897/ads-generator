27f78e52c378e948156a00320e2f00ffa706aafc
VikasThakur <vikasthakur1723@gmail.com>
2025-09-20 21:32:58 +0530

    complete the sidebar component

--- app/workspace/_components/AppSidebar.tsx
+++ app/workspace/_components/AppSidebar.tsx
@@ -1,23 +1,77 @@
+"use client"
 import React from 'react'
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { IoMdAddCircle } from "react-icons/io";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { IoVideocamOutline } from "react-icons/io5";
+import { LuVideotape } from "react-icons/lu";
+import { CiMoneyBill } from "react-icons/ci";
+import { IoSettingsOutline } from "react-icons/io5";
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
+const MenuItems = [
+  { name: 'Dashboard', icon: <LuLayoutDashboard />, link: '/workspace' },
+  { name: 'Create Ad', icon: <IoVideocamOutline />, link: '/workspace/create-ad' },
+  { name: 'My Video', icon: <LuVideotape />, link: '/workspace/my-video' },
+  { name: 'Billing', icon: <CiMoneyBill />, link: '/workspace/billing' },
+  { name: 'Settings', icon: <IoSettingsOutline />, link: '/workspace/Setting' },
+]
 
 const AppSidebar = () => {
+  const pathname = usePathname()
+
   return (
-     <Sidebar>
-      <SidebarHeader className={'flex item-center'} >
-        <Image src={'./logo.svg'} alt={'logo'} width={50} height={50} />
+    <Sidebar>
+      <SidebarHeader className="flex items-center my-3">
+        <Image src="/logo.svg" alt="logo" width={30} height={50} />
       </SidebarHeader>
+
+      <hr />
+
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <Button className="w-full mt-4 flex gap-2">
+            <IoMdAddCircle className="text-xl" />
+            Create New Ad Video
+          </Button>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-[14px] mb-2">Application</SidebarGroupLabel>
+          <SidebarContent>
+            <SidebarMenu className='space-y-2'>
+              {MenuItems.map((item, index) => (
+                <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild
+                    className={`text-[17px] ${
+                      pathname === item.link&&'bg-slate-200 text-black font-medium'
+                    }`}
+                  >
+                    <Link href={item.link}>
+                      {item.icon}
+                      <span>{item.name}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarContent>
+        </SidebarGroup>
       </SidebarContent>
+
       <SidebarFooter />
     </Sidebar>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}" // for Next.js app router
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
